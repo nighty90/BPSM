050d94837e64bf9bb00342d1a3e7df7521f34798
nighty90 <nighty.little.nightmare@gmail.com>
2022-10-11 13:13:23 +0100

    lecture06: improve get_s_acc.sh

--- Lecture06/get_s_acc.sh
+++ Lecture06/get_s_acc.sh
@@ -1,12 +1,22 @@
-#! /bin/bash
+#! usr/bin/bash
 unset IFS
 IFS=$'\t'
-while read q_acc s_acc id ali_len mismat gap q_s q_e s_s s_e evalue bit_score
+flag=1
+while read line
 do
 	if [ ${q_acc:0:1} == "#" ]
 	then
 		echo "ignore"
-	else
-		echo -e "${s_acc}" >> s_acc.txt
+		continue
 	fi
+	
+	if [ $flag -eq 1 ]
+	then
+		echo "s_acc" > s_acc.txt
+		flag=0
+	fi
+	
+	read q_acc s_acc id ali_len mismat gap q_s q_e s_s s_e evalue bit_score <<< ${line}
+	echo -e "${s_acc}" >> s_acc.txt
+	
 done < blastoutput2.out
